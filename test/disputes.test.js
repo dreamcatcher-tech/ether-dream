5e08d84b27a74c669c9182c284b7974b65e94e54
inverted_capital <evil_liskov@protonmail.com>
2023-08-25 17:01:16 +1200

    larger model timeout

--- test/disputes.test.js
+++ test/disputes.test.js
@@ -1,4 +1,26 @@
-describe('disputes', () => {
-  it.skip('disputes cannot be disputed')
-  it.skip('cannot dispute a packet')
-})
+// import { is, filters, and } from './machine.js'
+// import { expect } from 'chai'
+// import test from './testFactory.js'
+
+// describe('disputes', () => {
+//   describe('dispute a header', () => {
+//     test({
+//       toState: (state) =>
+//         state.matches('pending') &&
+//         is({ defundStarted: true, defundEnded: false })(state.context),
+//       filter: and(
+//         filters.skipPacketFunding,
+//         filters.skipTrading,
+//         filters.allowedStates('idle', 'open', 'defund', 'pending'),
+//         filters.dai
+//       ),
+//       verify: (sut) =>
+//         expect(sut.tests.defundExitAfterQa).to.have.been.calledOnce &&
+//         expect(sut.tests.defundInvalidStart).to.have.been.calledOnce &&
+//         expect(sut.tests.defundInvalidStop).to.have.been.calledOnce,
+//     })
+//   })
+//   it.skip('reverts if dispute window has passed')
+//   it.skip('disputes cannot be disputed')
+//   it.skip('cannot dispute a packet')
+// })
